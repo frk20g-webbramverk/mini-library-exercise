13b000b86947985a677744cc8b12d3934b9259c8
Christoffer Wallenberg <christoffer.wallenberg@zocom.se>
2021-03-01 10:35:23 +0100

    Added code

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <BooksList v-bind:books="books" />
+  </div>
+</template>
+
+<script>
+import books from './assets/childrensbooks.json';
+import BooksList from './components/bookList';
+
+export default {
+    components: {
+        BooksList
+    },
+    computed: {
+        books() {
+            return books.books;
+        }
+    }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
